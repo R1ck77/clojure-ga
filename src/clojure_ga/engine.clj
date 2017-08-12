da3c159dcaaa64482aaba6134266bbbfa0741e7a
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-08-12 20:12:22 +0200

    Add some tests on boundary conditions

--- src/clojure_ga/engine.clj
+++ src/clojure_ga/engine.clj
@@ -1,9 +1,14 @@
 (ns clojure-ga.engine)
 
+(defn- p-out-of-range? [p]
+  (or (< p 0) (> p 1)))
+
 (defn- validate [engine]
   (cond
     (nil? (:p-cross engine)) (throw (IllegalArgumentException. "missing cross probability"))
+    (p-out-of-range? (:p-cross engine)) (throw (IllegalArgumentException. "cross probability out of range"))
     (nil? (:p-mutation engine)) (throw (IllegalArgumentException. "missing mutation probability"))
+    (p-out-of-range? (:p-mutation engine)) (throw (IllegalArgumentException. "mutation probability out of range"))
     (nil? (:op-mutation engine)) (throw (IllegalArgumentException. "missing mutation function"))
     (nil? (:op-cross engine)) (throw (IllegalArgumentException. "missing crossing function"))
     :default engine))
--- test/clojure_ga/engine_test.clj
+++ test/clojure_ga/engine_test.clj
@@ -2,22 +2,30 @@
   (:require [clojure.test :refer :all]
             [clojure-ga.engine :as engine]))
 
-(def reasonable-default-arguments [:p-cross 0.75
-                                   :p-mutation 0.01
-                                   :op-cross (fn [a b] a)
-                                   :op-mutation (fn [a] a)])
+(def reasonable-default-arguments [:p-cross 0.75 :p-mutation 0.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
 
 (def missing-p-cross [:p-mutation 0.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
 (def missing-p-mutation [:p-cross 0.75 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
 (def missing-op-cross [:p-cross 0.75 :p-mutation 0.01 :op-mutation (fn [a] a)])
 (def missing-op-mutation [:p-cross 0.75 :p-mutation 0.01 :op-cross (fn [a b] a)])
 
+(def p-cross-too-high [:p-cross 1.75 :p-mutation 0.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
+(def p-cross-too-low [:p-cross -0.75 :p-mutation 0.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
+(def p-mutation-too-high [:p-cross 0.75 :p-mutation 1.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
+(def p-mutation-too-low [:p-cross 0.75 :p-mutation -0.01 :op-cross (fn [a b] a) :op-mutation (fn [a] a)])
+
 
 (deftest engine-creation
   (testing "the engine can be instantiated"
     (is (apply engine/create reasonable-default-arguments)))
-  (testing "missing parameters are met win an exception"
+  (testing "missing parameters are met with an exception"
     (is (thrown? IllegalArgumentException (apply engine/create missing-p-cross)))
     (is (thrown? IllegalArgumentException (apply engine/create missing-p-mutation)))
     (is (thrown? IllegalArgumentException (apply engine/create missing-op-cross)))
-    (is (thrown? IllegalArgumentException (apply engine/create missing-op-mutation)))))
+    (is (thrown? IllegalArgumentException (apply engine/create missing-op-mutation))))
+  (testing "invalid probabilities are also met with an exception"
+    (is (thrown? IllegalArgumentException (apply engine/create p-cross-too-high)))
+    (is (thrown? IllegalArgumentException (apply engine/create p-cross-too-low)))
+    (is (thrown? IllegalArgumentException (apply engine/create p-mutation-too-high)))
+    (is (thrown? IllegalArgumentException (apply engine/create p-mutation-too-low)))))
+
