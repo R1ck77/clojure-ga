31e127de402f7a8a0ee42315320908f75888d23c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-13 17:33:39 +0100

    WIP to fix a test

--- src/clojure_ga/fitness_proportionate_selection.clj
+++ src/clojure_ga/fitness_proportionate_selection.clj
@@ -1,5 +1,43 @@
 (ns clojure-ga.fitness-proportionate-selection)
 
+(defn- sort-by-fitness
+  [population score-function]
+  (sort-by #(- (first %))
+           (map (fn [chromosome]
+                  (vector (score-function chromosome) chromosome))
+                population)))
+
+(defn- accumulate-weights
+  [accumulator [weight chromosome]]
+  (let [[last-acc-value _] (last accumulator)
+        new-accumulated-difference (+ last-acc-value weight)]
+    (conj accumulator (vector new-accumulated-difference chromosome))))
+
+(defn- weighted-population-to-accumulated-weights
+  [weighted-population]
+  (let [last-chromosome (second (last weighted-population))]
+    (reduce accumulate-weights [[0 nil]] weighted-population)))
+
+(defn- pick-element-from-weighted-population
+  "Returns the selected chromosome using the fitness-proportionate-selection with the specified random value"
+  [weighted-population value]
+  (let [accumulated-weights (weighted-population-to-accumulated-weights weighted-population)
+        candidate-pair (first (drop-while #(>= value (first %)) accumulated-weights))
+        alternative-candidate-pair (last weighted-population)]
+    (second
+     (or candidate-pair
+         alternative-candidate-pair))))
+
+(defn- get-total-weight [weighted-population]
+  (reduce #(+ % (first %2)) 0 weighted-population))
+
+(defn- pick-element
+  "Return the selected chromosome from the population using the specified random value"
+  [population score-function random-value]
+  (let [weighted-population (sort-by-fitness population score-function)
+        total-weight (get-total-weight weighted-population)]
+    (pick-element-from-weighted-population weighted-population (* total-weight random-value))))
+
 (defn select
   "Select n non distinct elements with the Roulette Method
 
@@ -7,4 +45,4 @@ Use the score function with the element itself to get its fitness and the random
   [n population score-function random-generator]
   (if (empty? population)
     []
-    (vec (take n (repeat (first (reverse population)))))))
+    (take n (repeatedly #(pick-element population score-function (random-generator))))))
--- test/clojure_ga/fitness_proportionate_selection_test.clj
+++ test/clojure_ga/fitness_proportionate_selection_test.clj
@@ -13,11 +13,13 @@
 
 (defn- selection-to-random [total-elements index]
   (double
-   (+ (/ index total-elements)
-      (/ 1 (* 2 total-elements)))))
+   (/ index total-elements)))
 
 (defn- create-fake-random [n-elements selections]
   (create-iterator (map #(selection-to-random n-elements %) selections)))
+
+(defn- score-keyword [keyword]
+  (get {:a 1, :b 2, :c 3, :d 4, :e 5} keyword))
 
 (deftest test-select
   (testing "select from an empty population returns an empty list"
@@ -29,8 +31,16 @@
            (selection/select 4 [:a :b :c :d :e]
                              (fn [x] 0)
                              (create-fake-random 5 [0 2 4 1])))))
+  (testing "small case, with duplicates"
+    (is (= [:a :b :b :a]     ;;; expected: [[b 2] [a 3]]
+           (selection/select 4 [:a :b]
+                             score-keyword
+                             (create-fake-random 3 [2.1 0 1.999999 2.9])))))
   (testing "general case"
-    (is (= [:b :a :e :e :c]
-           (selection/select 4 [:a :b :c :d :e]
-                             (fn [x] (get {:a 1, :b 2, :c 3, :d 4, :e 5} x))
+    (is (= [:a :a :e :e :c]
+           (selection/select 4 
+                               ;;; [[:e 5] [:d 9] [:c 12] [:b 14] [:a 15]]
+                             score-keyword
                              (create-fake-random 15 [14 15 4 2 12])))))) 
+
+;;; -> [[:e 5] [:d 9] [:c 12] [:b 14] [:a 15]]
