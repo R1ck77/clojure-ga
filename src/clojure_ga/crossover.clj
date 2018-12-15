29b2d1efc5bde1f22f756eb3924a2a0deac1c1fa
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-16 00:08:09 +0100

    Change the way the crossover works
    It's simpler now, and the probability is accounted at another level

--- src/clojure_ga/crossover.clj
+++ src/clojure_ga/crossover.clj
@@ -1,15 +1,6 @@
 (ns clojure-ga.crossover)
 
-(defn- should-apply-operator? [random-f p-cross]
-  (<= (random-f) p-cross))
-
-(defn- attempt-cross [random-f p-cross cross-f a b]
-  (if (should-apply-operator? random-f p-cross)
-    (cross-f a b)
-    [a b]))
-
-(defn crossover [population {:keys [:random-f :p-cross :cross-f]}]
-  (let [cross-function (partial attempt-cross random-f p-cross cross-f)]
-    (doall
-     (mapcat #(apply cross-function %)
-             (partition 2 2 population)))))
+(defn crossover [population cross-function]
+  (doall
+   (mapcat #(apply cross-function %)
+           (partition 2 2 population))))
--- test/clojure_ga/crossover_test.clj
+++ test/clojure_ga/crossover_test.clj
@@ -5,83 +5,40 @@
 
 (deftest not-enough-elements
   (testing "doesn't invoke the cross operator on an empty population"
-    (crossover/crossover []
-                         {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                          :p-cross 1
-                          :cross-f (fn [_ _] (is false "operator invoked but not expected"))})
+    (crossover/crossover [] (fn [_ _] (is false "operator invoked but not expected")))
     (is true))
   (testing "returns an empty vector on an empty population"
     (is (= []
-           (crossover/crossover []
-                                {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                                 :p-cross 1
-                                 :cross-f (fn [_ _] (is false "operator invoked but not expected"))}))))  
+           (crossover/crossover [] (fn [_ _] (is false "operator invoked but not expected"))))))  
   (testing "doesn't invoke the cross operator on a population of one element"
-    (crossover/crossover [:chromosome]
-                         {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                          :p-cross 1
-                          :cross-f (fn [_ _] (is false "operator invoked but not expected"))}))
+    (crossover/crossover [:chromosome] (fn [_ _] (is false "operator invoked but not expected"))))
   (testing "returns an empty vector on a population of one element"
     (is (= []
-           (crossover/crossover [:chromosome]
-                                {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                                 :p-cross 1
-                                 :cross-f (fn [_ _] (is false "operator invoked but not expected"))})))))
+           (crossover/crossover [:chromosome] (fn [_ _] (is false "operator invoked but not expected")))))))
 
 (defn- crossover-on-two-elements [counter p-cross expected-random-value]
   (let [conversion {:chromosome1 :result1
                     :chromosome2 :result2}]
     (crossover/crossover [:chromosome1 :chromosome2]
-                         {:random-f #(identity expected-random-value)
-                          :p-cross p-cross
-                          :cross-f (fn [a b]
-                                     (swap! counter inc)
-                                     (is (and (= :chromosome1 a)
-                                              (= :chromosome2 b)))
-                                     (vector (get conversion a)
-                                             (get conversion b)))})))
+                         (fn [a b]
+                           (swap! counter inc)
+                           (is (and (= :chromosome1 a)
+                                    (= :chromosome2 b)))
+                           (vector (get conversion a)
+                                   (get conversion b))))))
 (deftest one-pair
-  (testing "in a two chromosomes population with p cross 1, crossover applies the operator once on the chromosomes"
+  (testing "crossover applies the operator once on the chromosomes"
     (let [counter (atom 0)]
       (crossover-on-two-elements counter 1 0.5)
       (is (= 1 @counter) "Crossover operator not executed the expected number of times")))
-  (testing "in a two chromosomes population with p cross a, crossover applies the operator only if the random generator return b <=a"
-    (let [counter (atom 0)]
-      (crossover-on-two-elements counter 0.3 0.2)    
-      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
-      (reset! counter 0)
-
-      (crossover-on-two-elements counter 0.3 0.4)    
-      (is (= 0 @counter) "Crossover operator not executed the expected number of times")
-      (reset! counter 0)
-
-      (crossover-on-two-elements counter 0.3 0.3)    
-      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
-      (reset! counter 0)
-
-      (crossover-on-two-elements counter 0 0)
-      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
-      (reset! counter 0)))
-  (testing "for two chromosomes returns the output of the crossover function when the crossover is applied"
+  (testing "for two chromosomes returns the output of the crossover function"
     (let [counter (atom 0)]
       (is (= [:result1 :result2]
-             (crossover-on-two-elements counter 1 1)))))
-  (testing "for two chromosomes returns the original pair when the crossover is not applied"
-    (let [counter (atom 0)]
-      (is (= [:chromosome1 :chromosome2]
-             (crossover-on-two-elements counter 0 1))))))
+             (crossover-on-two-elements counter 1 1))))))
 
 (deftest multiple-values
-  (testing "general case: various probabilities and multiple chromosomes"
-    (is (= [11 -8 3 4 5 6 17 -2]
+  (testing "general case: multiple chromosomes"
+    (is (= [11 -8 13 -6 15 -4 17 -2]
            (crossover/crossover [1 2 3 4 5 6 7 8]
-                                {:random-f (utils/create-iterator [0.2 0.8 0.8 0.1])
-                                 :p-cross 0.5
-                                 :cross-f (fn [a b] (vector (+ a 10) (- b 10)))}))))
-  (testing "general case: various probabilities and multiple chromosomes, odd elements"
-    (is (= [11 -8 3 4 5 6 17 -2]
-           (crossover/crossover [1 2 3 4 5 6 7 8 9]
-                                {:random-f (utils/create-iterator [0.2 0.8 0.8 0.5])
-                                 :p-cross 0.5
-                                 :cross-f (fn [a b] (vector (+ a 10) (- b 10)))})))))
+                                (fn [a b] (vector (+ a 10) (- b 10))))))))
 
