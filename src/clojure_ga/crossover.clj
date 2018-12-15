411ac6b3325d95ea913c27317a2804d21a04d04b
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-15 01:07:37 +0100

    Add tests for some crossover characterization

--- src/clojure_ga/crossover.clj
+++ src/clojure_ga/crossover.clj
@@ -1,5 +1,14 @@
 (ns clojure-ga.crossover)
 
-(defn apply [population parameters]
-  
-)
+(defn- should-apply-operator? [random-f p-cross]
+  (<= (random-f) p-cross))
+
+(defn- attempt-cross [random-f p-cross cross-f a b]
+  (if (should-apply-operator? random-f p-cross)
+    (cross-f a b)))
+
+(defn crossover [population {:keys [:random-f :p-cross :cross-f]}]
+  (let [cross-function (partial attempt-cross random-f p-cross cross-f)]
+    (doall
+     (map #(apply cross-function %)
+          (partition 2 2 population)))))
--- test/clojure_ga/crossover_test.clj
+++ test/clojure_ga/crossover_test.clj
@@ -1,16 +1,60 @@
 (ns clojure-ga.crossover-test
   (:require [clojure.test :refer :all]
+            [clojure-ga.utils :as utils]
             [clojure-ga.crossover :as crossover]))
 
-(deftest crossover-behavior
+(deftest crossover-not-enough-elements
   (testing "crossover doesn't invoke the cross operator on an empty population"
-    (crossover/apply []
-                     {:random-generator #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                      :cross-probability 1
-                      :cross-operator (fn [_ _] (is false "operator invoked but not expected"))})
+    (crossover/crossover []
+                         {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
+                          :p-cross 1
+                          :cross-f (fn [_ _] (is false "operator invoked but not expected"))})
     (is true))
+  (testing "crossover returns an empty vector on an empty population"
+    (is (= []
+           (crossover/crossover []
+                                {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
+                                 :p-cross 1
+                                 :cross-f (fn [_ _] (is false "operator invoked but not expected"))}))))  
   (testing "crossover doesn't invoke the cross operator on a population of one element"
-    (crossover/apply [:chromosome]
-                     {:random-generator #(throw (IllegalStateException. "Unexpected execution of the random generator"))
-                      :cross-probability 1
-                      :cross-operator (fn [_ _] (is false "operator invoked but not expected"))})))
+    (crossover/crossover [:chromosome]
+                         {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
+                          :p-cross 1
+                          :cross-f (fn [_ _] (is false "operator invoked but not expected"))}))
+  (testing "crossover returns an empty vector on a population of one element"
+    (is (= []
+           (crossover/crossover [:chromosome]
+                                {:random-f #(throw (IllegalStateException. "Unexpected execution of the random generator"))
+                                 :p-cross 1
+                                 :cross-f (fn [_ _] (is false "operator invoked but not expected"))})))))
+
+(defn- crossover-on-two-elements [counter p-cross expected-random-value]
+  (crossover/crossover [:chromosome1 :chromosome2]
+                       {:random-f #(identity expected-random-value)
+                        :p-cross p-cross
+                        :cross-f (fn [a b]
+                                   (swap! counter inc)
+                                   (is (and (= :chromosome1 a)
+                                            (= :chromosome2 b))))}))
+(deftest crossover-one-pair
+  (testing "in a two chromosomes population with p cross 1, crossover applies the operator once on the chromosomes"
+    (let [counter (atom 0)]
+      (crossover-on-two-elements counter 1 0.5)
+      (is (= 1 @counter) "Crossover operator not executed the expected number of times")))
+  (testing "in a two chromosomes population with p cross a, crossover applies the operator only if the random generator return b <=a"
+    (let [counter (atom 0)]
+      (crossover-on-two-elements counter 0.3 0.2)    
+      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
+      (reset! counter 0)
+
+      (crossover-on-two-elements counter 0.3 0.4)    
+      (is (= 0 @counter) "Crossover operator not executed the expected number of times")
+      (reset! counter 0)
+
+      (crossover-on-two-elements counter 0.3 0.3)    
+      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
+      (reset! counter 0)
+
+      (crossover-on-two-elements counter 0 0)
+      (is (= 1 @counter) "Crossover operator not executed the expected number of times")
+      (reset! counter 0))))
