d60c2eab86b83d91d02a21339c14239630b86d0c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-16 15:39:11 +0100

    Refactor the tests and classes

--- src/clojure_ga/simple.clj
+++ src/clojure_ga/simple.clj
@@ -3,6 +3,16 @@
             [clojure-ga.crossover :as crossover]
             [clojure-ga.mutation :as mutation]))
 
-(defn evolve [population {:keys [:fitness-f :cross-f :mutation-f :random-f]}]  
-  (mutation/mutation (selection/select (count population) population fitness-f random-f)
-                     mutation-f))
+(defprotocol Evolver
+  (evolve [this population] "evolve a single step of a population"))
+
+(defrecord SimpleGA [fitness-f crossover-f mutation-f])
+
+(extend-type SimpleGA
+  Evolver
+  (evolve [this population]
+    (mutation/mutation (selection/select (count population)
+                                         population
+                                         (get this :fitness-f)
+                                         rand)
+                       (get this :mutation-f))))
--- test/clojure_ga/simple_test.clj
+++ test/clojure_ga/simple_test.clj
@@ -3,57 +3,44 @@
             [clojure-ga.simple :as simple]
             [clojure-ga.config :as config]))
 
-(defn- counter-function
-  "Returns a function that just increases the counter"
-  [counter]
-  (fn [& args]
-    (swap! counter inc)))
 
 (defn- throwing-function [& args]
   (is false "unexpected execution"))
 
-(defn- create-mock-config [counter]
-  (config/create-config :op-generation (counter-function counter)
-                        :op-fitness (counter-function counter)
-                        :op-cross (counter-function counter)
-                        :op-mutation (counter-function counter)))
+(defn- create-throwing-instance []
+  (simple/->SimpleGA throwing-function throwing-function throwing-function))
+
 
 (deftest empty-population
   (testing "returns an empty vector for an empty population"
-    (is (= [] (simple/evolve [] {:mutation-f throwing-function
-                                 :cross-f throwing-function
-                                 :fitness-f throwing-function
-                                 :random-f throwing-function}))))
+    (is (= [] (simple/evolve (create-throwing-instance) []))))
   (testing "doesn't invoke any function"
-    (simple/evolve [] {:mutation-f throwing-function
-                       :cross-f throwing-function
-                       :fitness-f throwing-function
-                       :random-f throwing-function})
+    (simple/evolve (create-throwing-instance) [])
     (is true)))
 
 (deftest single-chromosome
   (testing "a step involves the execution of fitness and mutation operations, in order"
     (let [operations (atom [])]
-      (simple/evolve [:a] {:fitness-f (fn [chromosome]
-                                        (swap! operations #(conj % (str chromosome "-fitness")))
-                                        1)
-                           :cross-f throwing-function
-                           :mutation-f (fn [chromosome]
-                                         (swap! operations #(conj % (str chromosome "-mutation"))))
-                           :random-f (fn [] 0.5)})
+      (let [fitness-f (fn [chromosome]
+                        (swap! operations #(conj % (str chromosome "-fitness")))
+                        1)
+            mutation-f (fn [chromosome]
+                         (swap! operations #(conj % (str chromosome "-mutation"))))
+            simpleGa (simple/->SimpleGA fitness-f throwing-function mutation-f)]
+       (simple/evolve simpleGa [:a]))
       (is (= [":a-fitness" ":a-mutation"] @operations)))))
 
 (deftest two-chromosomes
   (testing "a step involves the execution of fitness, crossover and mutation, in order"
     (let [operations (atom [])]
-      (simple/evolve [:a :b] {:fitness-f (fn [chromosome]
-                                        (swap! operations #(conj % (str chromosome "-fitness")))
-                                        1)
-                              :cross-f (fn [chromosome-1 chromosome-2]
-                                         (swap! operations #(conj % (str chromosome-1 "-crossed-" chromosome-2)))
-                                         [chromosome-1 chromosome-2])
-                           :mutation-f (fn [chromosome]
-                                         (swap! operations #(conj % (str chromosome "-mutation")))
-                                         chromosome)
-                           :random-f (fn [] 0.5)})
+      (let [simpleGa (simple/map->SimpleGA {:fitness-f (fn [chromosome]
+                                      (swap! operations #(conj % (str chromosome "-fitness")))
+                                      1)
+                         :cross-f (fn [chromosome-1 chromosome-2]
+                                    (swap! operations #(conj % (str chromosome-1 "-crossed-" chromosome-2)))
+                                    [chromosome-1 chromosome-2])
+                         :mutation-f (fn [chromosome]
+                                       (swap! operations #(conj % (str chromosome "-mutation")))
+                                       chromosome)})]
+        (simple/evolve simpleGa [:a :b]))      
       (is (= [":a-fitness" ":b-fitness" ":a-crossed-:b" ":a-mutation" ":b-mutation"] @operations)))))
