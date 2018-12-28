b0c3ab90fdd8f780f4cc1dfda0811a592fab0070
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-28 12:17:00 +0100

    Test words-seq

--- test/clojure_ga/words/words_test.clj
+++ test/clojure_ga/words/words_test.clj
@@ -8,12 +8,18 @@
 (deftest test-words-seq
   (testing "empty string"
     (is (= '() (words/words-seq ""))))
-  (testing "single word, various scenarios"
+  (testing "single word"
     (is (= '([0 3]) (words/words-seq "foo")))
     (is (= '([0 3]) (words/words-seq "foo ")))
     (is (= '([1 4]) (words/words-seq " foo")))    
     (is (= '([1 4]) (words/words-seq " foo ")))
-    (is (= '([2 5]) (words/words-seq "  foo   ")))))
+    (is (= '([2 5]) (words/words-seq "  foo   "))))
+  (testing "multiple word"
+    (is (= '([0 3] [4 7]) (words/words-seq "foo bar")))
+    (is (= '([0 3] [4 7]) (words/words-seq "foo bar ")))
+    (is (= '([1 4] [6 9]) (words/words-seq " foo  bar")))    
+    (is (= '([1 4] [5 8]) (words/words-seq " foo bar ")))
+    (is (= '([2 5] [8 11]) (words/words-seq "  foo   bar  ")))))
 
 (deftest test-pick-random-words-sequence
   (comment (testing "a general case"
