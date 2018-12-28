5ad7399e356dbd89c028be6e33dc4a6c3bdbca37
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-28 10:38:44 +0100

    Start simpler example to profile the GA
    The experimental automatic code generation is way too complex to make up what's a GA issue and what's a problem issue.

---
+++ src/clojure_ga/words/words.clj
@@ -0,0 +1,5 @@
+(ns clojure-ga.words.words
+  (:require [clojure-ga.simple :as simple]
+            [clojure-ga.fitness-proportionate-selection :as fitness]
+            [clojure-ga.crossover :as crossover]
+            [clojure-ga.mutation :as mutation]))
