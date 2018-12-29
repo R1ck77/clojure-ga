bda10536babc7d0a330d0fd62ee0d2382bee4bd8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-29 23:17:31 +0100

    Tournment selector in place

--- src/clojure_ga/tournament_selection.clj
+++ src/clojure_ga/tournament_selection.clj
@@ -1,14 +1,46 @@
 (ns clojure-ga.tournament-selection
   (:require [clojure-ga.selector :as selector]))
 
-(defrecord TournmentSelector [rank fitness-function])
+(defrecord TournmentSelector [rank fitness-function rand-nth])
 
-(defn create-selector [rank fitness-function]
+(defn create-selector [rank fitness-function rand-nth]
   (if (< rank 1)
     (throw (IllegalArgumentException. "The rank must be positive"))
-    (->TournmentSelector rank fitness-function)))
+    (->TournmentSelector rank fitness-function rand-nth)))
+
+(defn- pick-group [population rank rand-nth]
+  (take rank (repeatedly #(rand-nth population))))
+
+(defn- pick-groups [population pick-function]
+  (take (count population) (repeatedly pick-function)))
+
+(defn- score-group [group fitness-function]
+  (map #(vector % (fitness-function %)) group))
+
+(defn- compare-scored-chromosomes [[_ best-score :as previous-best]
+                                   [_ current-score :as candidate-best]]
+  (if (> current-score best-score)
+    candidate-best
+    previous-best))
+
+(defn- best-of-group [scored-group]
+  (first
+   (reduce compare-scored-chromosomes
+           (first scored-group)
+           scored-group)))
+
+(defn- pick-element [population rank fitness-function rand-nth]
+  (let [pick-function #(pick-group population rank rand-nth)
+        score-group-function #(score-group % fitness-function)
+        groups (pick-groups population pick-function)]
+    (map best-of-group
+         (map score-group-function groups))))
 
 (extend-type TournmentSelector
   selector/Selector
   (select [this population]
-    []))
+    (let [rank (:rank this)
+          fitness-function (:fitness-function this)
+          rand-nth (:rand-nth this)]
+      (take (count population)
+            (pick-element population rank fitness-function rand-nth)))))
--- test/clojure_ga/tournament_selection_test.clj
+++ test/clojure_ga/tournament_selection_test.clj
@@ -6,18 +6,58 @@
 
 (deftest tournment-selector-constructor-test
   (testing "sunny day"
-    (let [correct-instance? (instance? clojure_ga.tournament_selection.TournmentSelector (ts/create-selector 1 (fn [_] 12)))]
+    (let [correct-instance? (instance? clojure_ga.tournament_selection.TournmentSelector (ts/create-selector 1 (fn [_] 12) rand-nth))]
       (is correct-instance?)))
   (testing "fitness function not invoked during construction"
-    (is (not (nil? (ts/create-selector 1 (fn [_] (assert false)))))))
+    (is (not (nil? (ts/create-selector 1 (fn [_] (assert false)) rand-nth)))))
   (testing "throws if rank < 1"
     (is (thrown? IllegalArgumentException (ts/create-selector 0 (fn [_] 12))))
     (is (thrown? IllegalArgumentException (ts/create-selector -1 (fn [_] 12)))))
   (testing "arguments assigned correctly"
-    (let [selector (ts/create-selector 12 identity)]
+    (let [selector (ts/create-selector 12 identity rand-nth)]
       (is (= 12 (get selector :rank)))
       (is (= identity (get selector :fitness-function))))))
 
+(defn- mock-rand-nth
+  "Like rand-nth, but the game is rigged"
+  [& indices]
+  (let [next-picks (atom indices)]
+    (fn [coll]
+      (let [current (first @next-picks)
+            next (swap! next-picks rest)]
+        (nth coll current)))))
+
+(defmacro repeat-testXY [inputs ranks]
+  (conj (for [input inputs
+              rank ranks]    
+          `(is (= (count ~input)
+                  (count (selector/select (ts/create-selector ~rank identity rand-nth)
+                                          ~input)))))
+        'do))
+
 (deftest selector-test
-  (testing "empty population → empty selection"
-    (is (= [] (selector/select (ts/create-selector 12 identity) [])))))
+  (comment (testing "empty population → empty selection"
+     (is (= [] (selector/select (ts/create-selector 12 identity rand-nth) []))))
+   (testing "single element in the population → same element out"
+     (is (= [44] (selector/select (ts/create-selector 12 identity rand-nth) [44]))))
+   (testing "n elements in → n elements out, for any valid rank"
+     (repeat-test [[] [0] [0 1] [0 1 2] [0 1 2 3] [0 1 2 3 4]]
+                  (range 1 10)))
+   (testing "many elements, rank 1"
+     (let [fake-rand-int (mock-rand-nth 0 1 3 0 4 3)]
+       (is (= [0 1 3 0 4 3]
+              (selector/select (ts/create-selector 1 identity fake-rand-int)
+                               [0 1 2 3 4 5]))))))
+  (testing "many elements, rank 2"
+    (let [fake-rand-int (mock-rand-nth 0 1 3 0 4 3 2 2 0 3 2 4)]
+      (is (= [1 3 4 2 3 4]
+             (selector/select (ts/create-selector 2 identity fake-rand-int)
+                              [0 1 2 3 4 5])))))
+  (testing "in case of tie, the first biggest element in a group wins"
+    (let [fake-rand-int (mock-rand-nth 0 1 2 3
+                                       2 0 3 0
+                                       0 2 0 3
+                                       3 0 2 0)]
+      (is (= [:b :c :a :d]
+             (selector/select (ts/create-selector 4 {:a 1 :b 2 :c 1 :d 1} fake-rand-int)
+                              [:a :b :c :d]))))))
