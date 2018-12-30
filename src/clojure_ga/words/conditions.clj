45b67cf3f8333d8a2d79839ac7734b4af1c1d40e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-30 19:13:39 +0100

    Add condition with side effects

--- src/clojure_ga/words/conditions.clj
+++ src/clojure_ga/words/conditions.clj
@@ -13,3 +13,15 @@
   (should-end? [this _]
     (>= (swap! (:atom-countdown this) dec) 0)))
 
+(defrecord SideEffectCondition [end-condition side-effect-f])
+
+;;; TODO/FIXME all nice and good, but frankly a simple function for the condition will do…
+(defn create-side-effect-condition [condition side-effect-f]
+  (->SideEffectCondition condition side-effect-f))
+
+(extend-protocol EndCondition
+  SideEffectCondition
+  (should-end? [this population]
+    ((:side-effect-f this) population)
+    (should-end? (:end-condition this) population)))
+
--- test/clojure_ga/conditions_test.clj
+++ test/clojure_ga/conditions_test.clj
@@ -3,6 +3,7 @@
             [clojure-ga.conditions :as conditions]))
 
 (def fake-population [:a :b :c :d :e])
+(def other-fake-population [1 2 3 4])
 
 (deftest test-create-count-end-condition
   (testing "0 times condition returns always false"
@@ -24,3 +25,35 @@
                    (conditions/should-end? counter-condition fake-population)
                    (conditions/should-end? counter-condition fake-population)
                    (conditions/should-end? counter-condition fake-population)))))))
+
+(deftest test-create-side-effect-condition
+  (testing "when used as condition, invokes the side effect function and returns true if the condition is true"
+    (let [side-effect-invoked (atom false)
+          side-effect-condition (conditions/create-side-effect-condition (reify conditions/EndCondition (should-end? [this population] true))
+                                                                         (fn [population] (reset! side-effect-invoked fake-population)))]      
+      (is (conditions/should-end? side-effect-condition fake-population))
+      (is (identical? fake-population @side-effect-invoked))))
+  (testing "when used as condition, invokes the side effect function and returns false if the condition is false"
+    (let [side-effect-invoked (atom false)
+          side-effect-condition (conditions/create-side-effect-condition (reify conditions/EndCondition (should-end? [this population] false))
+                                                                         (fn [population] (reset! side-effect-invoked fake-population)))]      
+      (is (not (conditions/should-end? side-effect-condition fake-population)))
+      (is (identical? fake-population @side-effect-invoked))))
+  (testing "both the side effect and the condition are called on the population each time"
+    (let [condition-counter (atom nil)
+          side-effect-counter (atom nil)
+          expected-result (atom false)
+          counter-condition (reify conditions/EndCondition
+                              (should-end? [this population]
+                                (reset! condition-counter population)
+                                @expected-result))
+          side-effect-f #(reset! side-effect-counter %)
+          side-effect-condition (conditions/create-side-effect-condition counter-condition
+                                                                         side-effect-f)]
+      (is (= false (conditions/should-end? side-effect-condition fake-population)))
+      (is (identical? @condition-counter fake-population))
+      (is (identical? @side-effect-counter fake-population))
+      (reset! expected-result true)
+      (is (= true (conditions/should-end? side-effect-condition other-fake-population)))
+      (is (identical? @condition-counter other-fake-population))
+      (is (identical? @side-effect-counter other-fake-population)))))
