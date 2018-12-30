e2f4b8dd24f59472d1a3081dfa8440177611d103
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-30 11:43:16 +0100

    Fix printing of the demo RED

--- src/clojure_ga/core.clj
+++ src/clojure_ga/core.clj
@@ -8,6 +8,7 @@
   (let [results (words/simulation (Integer/valueOf (or (first args) 10))
                                   (Integer/valueOf (or (second args) 100))
                                   (Integer/valueOf (or (get args 2) 1000)))]
-    (map (fn [[score result]]
-           (println (str score "->" result))) (sort-by first results)))
+    (doall
+     (map (fn [[score result]]
+            (println (str score " : " (apply str result)))) (sort-by first results))))
   (shutdown-agents))
