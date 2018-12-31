45eb04ff1f2909bd346f322fa13fc622666340a9
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-12-31 11:43:32 +0100

    Add a mean and standard deviation functions

--- src/clojure_ga/statistics.clj
+++ src/clojure_ga/statistics.clj
@@ -1,9 +1,17 @@
 (ns clojure-ga.statistics)
 
+(defn- std-dev [sum values]
+  (let [N (count values)
+        squares (reduce #(+ % (* %2 %2)) 0 values)]
+    (Math/sqrt (/ (- (* N squares) (* sum sum))
+                  (* N (- N 1))))))
+
 (defn mean-std-dev [values]
-  (let [N (count values)]
+  (let [N (count values)
+        sum (reduce + values)]
     (if (< N 2)    
       (throw (ArithmeticException. ))
-      (vector (/ (reduce + values) N) 0))))
+      (vector (/ sum N)
+              (std-dev sum values)))))
 
  
--- test/clojure_ga/statistics_test.clj
+++ test/clojure_ga/statistics_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [clojure-ga.statistics :as stats]))
 
+(defn- about [x y eps]
+  (<= (Math/abs (- x y)) eps))
+
+(deftest about-test
+  (testing "couple of basic cases"
+    (is (about 1 2 2))
+    (is (not (about 1 2 0.5)))
+    (is (about 1 1 0))))
+
 (deftest mean-std-dev-test
   (testing "throws if there are less than two values"
     (is (thrown? ArithmeticException (stats/mean-std-dev [])))
@@ -10,5 +19,14 @@
     (is (= 12 (first (stats/mean-std-dev [12 12]))))
     (is (= 12 (first (stats/mean-std-dev [12 12 12 12 12])))))
   (testing "average, general case"
-    (is (= 4 (first (stats/mean-std-dev [2 4 6]))))))
+    (is (= 4 (first (stats/mean-std-dev [2 4 6])))))
+  (testing "the standard deviation of numbers all the same is 0"
+    (is (= 0.0 (second (stats/mean-std-dev [12 12]))))
+    (is (= 0.0 (second (stats/mean-std-dev [12 12 12 12 12])))))
+  (testing "the standard deviation doesn't depend from order"
+    (let [a-result (second (stats/mean-std-dev [1 2 3 4 5]))]
+      (is (about a-result (second (stats/mean-std-dev [5 4 3 2 1])) 1e-5))
+      (is (about a-result (second (stats/mean-std-dev [4 5 3 1 2])) 1e-5))))
+  (testing "general case"
+    (is (about 1.5811388300841898 (second (stats/mean-std-dev [1 2 3 4 5])) 1e-6))))
 
