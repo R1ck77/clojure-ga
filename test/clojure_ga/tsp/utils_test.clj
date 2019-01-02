c0ebdc05d0772d9ce49196eafbea8f1549c3aa25
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-01-02 21:13:56 +0100

    UT of subpaths replacement (WIP)

--- test/clojure_ga/tsp/utils_test.clj
+++ test/clojure_ga/tsp/utils_test.clj
@@ -18,3 +18,17 @@
   (testing "no common subsets"
     (is (= []
            (utils/common-subsets [:b :d :a :c] [:a :b :c :d])))))
+
+(deftest test-replace-subset
+   (testing "2 elements subset, start and end"
+    (is (= [[:c :b :a :d] [:d :a :b :c]]
+           (utils/replace-subset [:b :c :a :d] [:d :a :c :b] #{:c :b}))))
+  (testing "2 elements subset, middle"
+    (is (= [[:a :b :c :d] [:d :c :b :a]]
+           (utils/replace-subset [:a :c :b :d] [:d :b :c :a] #{:c :b}))))
+  (testing "general case"
+    (is (= [[:a :b :c :d :e :f]
+            [:f :e :d :c :b :a]]
+           (utils/replace-subset [:a :e :d :c :b :f]
+                                 [:f :b :c :d :e :a]
+                                 #{:b :c :d :e})))))
