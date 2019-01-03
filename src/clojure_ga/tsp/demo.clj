a37dc50dacff145f23e3b2544bdebd8410e2a92f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-01-03 13:32:00 +0100

    Better TSP parametrization

--- src/clojure_ga/tsp/demo.clj
+++ src/clojure_ga/tsp/demo.clj
@@ -13,9 +13,9 @@
 
 (defn gen-cities
   ([N] (gen-cities N rand))
-  ([N rand]
+  ([N rand-f]
    (if (> N 1)
-     (vec (repeatedly N #(vector (rand) (rand))))
+     (vec (repeatedly N #(vector (rand-f) (rand-f))))
      (throw (IllegalArgumentException. "N must be > 1")))))
 
 (defn- distance [[ax ay] [bx by]]
@@ -28,49 +28,49 @@
   (apply + (map #(apply distance %)
                 (partition 2 1 (map #(get cities %)  order)))))
 
-(defn create-crossover-operator []
+(defn create-crossover-operator [crossover-p rand-f rand-nth-f]
   (let [crossover-function (fn [a b]                                        
-                             (utils/cross-sequences a b rand-nth))]
+                             (utils/cross-sequences a b rand-nth-f))]
     (crossover/create-classic-crossover crossover-function
-                                        tsp-crossover-probability
-                                        rand)))
+                                        crossover-p
+                                        rand-f)))
 
-(defn exchange-elements [chromosome rand-int]
-  (let [a (rand-int (count chromosome))
+(defn exchange-elements [chromosome rand-int-f]
+  (let [a (rand-int-f (count chromosome))
         a-element (get chromosome a)
-        b (rand-int (count chromosome))
+        b (rand-int-f (count chromosome))
         b-element (get chromosome b)]
     (assoc (assoc chromosome a b-element) b a-element)))
 
 (defn create-mutation-operator
-  ([] (create-mutation-operator rand rand-int))
-  ([rand rand-int]  
+  ([mutation-p rand-f rand-int-f]  
    (let [mutation-f (fn [chromosome]
-                      (if (< (rand) tsp-mutation-probability)
-                        (exchange-elements chromosome rand-int)
-                        chromosome))]
+                      (let [rand-num (rand-f)]
+                        (if (< rand-num mutation-p)
+                               (exchange-elements chromosome rand-int-f)
+                               chromosome)))]
      (mutation/->SimpleMutation mutation-f))))
 
-(defn- remove-from-collection [x-int rand-int]
-  (let [index (rand-int (count x-int))]
+(defn- remove-from-collection [x-int rand-int-f]
+  (let [index (rand-int-f (count x-int))]
     (vector (get x-int index)
             (vec (concat (subvec x-int 0 index)
                      (subvec x-int (inc index)
                              (count x-int)))))))
 
-(defn new-random-route [N rand-int]
+(defn new-random-route [N rand-int-f]
   (let [seed (atom (vec (range N)))
         result (atom [])]
     (while (not (empty? @seed))
-      (let [[next next-seed] (remove-from-collection @seed rand-int)]
+      (let [[next next-seed] (remove-from-collection @seed rand-int-f)]
         (reset! seed next-seed)
         (swap! result #(conj % next))))
     @result))
 
-(defn create-tournament-selector [cities]
-  (tournament/create-selector tsp-tournament-selector-rank
+(defn create-tournament-selector [rank cities rand-nth-f]
+  (tournament/create-selector rank
                               (fn [route] (- (travel-length cities (vec route))))
-                              rand-nth))
+                              rand-nth-f))
 
 ;;; TODO/FIXME repeated in the words demo
 (defn- population-stats [scores]
@@ -86,14 +86,14 @@
         (counter-f population)))))
 
 (defn simulation
-  [N generations population-size]
-  (let [cities (gen-cities N rand)
-        evolver (simple/->SimpleGA (create-tournament-selector cities)
-                                   (create-crossover-operator)
-                                   (create-mutation-operator))]
+  [cities generations population-size {:keys [mutation-p crossover-p tournament-rank
+                                              rand-f rand-nth-f rand-int-f] :as parameters}]
+  (let [evolver (simple/->SimpleGA (create-tournament-selector tournament-rank cities rand-nth-f)
+                                   (create-crossover-operator crossover-p rand-f rand-nth-f)
+                                   (create-mutation-operator mutation-p rand-f rand-int-f))]
     (println "TODO: put the challenge here somehow")
     (let [simulation (simple/->SimpleSimulation evolver (create-countdown cities generations))
-          population (repeatedly population-size #(new-random-route N rand-int))]
+          population (repeatedly population-size #(new-random-route (count cities) rand-int-f))]
       (let [best-scored-results (sort-by first (map #(vector (travel-length cities %) %) (simple/evolve-while simulation population)))
             best-results (map second best-scored-results)
             best-scores (take 10 (map first best-scored-results))]
@@ -103,3 +103,14 @@
         (dorun (map #(let [city (get cities %)]
                        (println (first city) (second city) ))
                     (first best-results)))))))
+
+(defn simple-simulation [N generations population-size]
+  (simulation (gen-cities N rand)
+              generations
+              population-size
+              {:mutation-p tsp-mutation-probability
+               :crossover-p tsp-crossover-probability
+               :tournament-rank tsp-tournament-selector-rank
+               :rand-f rand
+               :rand-int-f rand-int
+               :rand-nth-f rand-nth}))
